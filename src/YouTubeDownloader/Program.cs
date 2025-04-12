bcfae22a27e84d009e8960442d3d532cb091b5c8
Sejba <sejba@yahoo.com>
2025-04-13 01:04:09 +0200

    cleanup

--- src/YouTubeDownloader/Program.cs
+++ src/YouTubeDownloader/Program.cs
@@ -3,43 +3,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using YouTubeDownloader.Services;
 using YoutubeExplode;
-using YoutubeExplode.Common;
 
-namespace YouTubeDownloader
+namespace YouTubeDownloader;
+
+class Program
 {
-    class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        var f = args.FirstOrDefault() ?? "";
+        var p = args.Skip(1).FirstOrDefault() ?? "h:\\yt\\videos";
+        var s = int.Parse(args.Skip(2).FirstOrDefault() ?? "1080");
+
+        Console.WriteLine("Welcome to YouTube Downloader!");
+        var videoDownloadService = new VideoDownloadService();
+
+        //var go = false;
+        if (f.Contains("playlist"))
         {
-            var f = args.FirstOrDefault() ?? "";
-            var p = args.Skip(1).FirstOrDefault() ?? "h:\\yt\\videos";
-            var s = int.Parse(args.Skip(2).FirstOrDefault() ?? "1080");
+            var youtube = new YoutubeClient();
 
-            Console.WriteLine("Welcome to YouTube Downloader!");
-            var videoDownloadService = new VideoDownloadService();
-
-            //var go = false;
-            if (f.Contains("playlist"))
+            await foreach (var video in youtube.Playlists.GetVideosAsync(f))
             {
-                var youtube = new YoutubeClient();
+                // if (video.Url == "")
+                //     go = true;
 
-                await foreach (var video in youtube.Playlists.GetVideosAsync(f))
-                {
-                    // if (video.Url == "")
-                    //     go = true;
+                // if (!go) continue;
 
-                    // if (!go) continue;
-
-                    Console.WriteLine($"Downloading {video.Url}");
-                    await videoDownloadService.DownloadVideo(video.Url, p, s);
-                    await Task.Delay(10000);
-                }
-
-                return;
+                Console.WriteLine($"Downloading {video.Url}");
+                await videoDownloadService.DownloadVideo(video.Url, p, s);
+                await Task.Delay(10000);
             }
 
-
-            await videoDownloadService.DownloadVideo(f, p, s);
+            return;
         }
+
+        await videoDownloadService.DownloadVideo(f, p, s);
     }
 }
--- src/YouTubeDownloader/Services/VideoDownloadService.cs
+++ src/YouTubeDownloader/Services/VideoDownloadService.cs
@@ -4,119 +4,110 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 
-namespace YouTubeDownloader.Services
+namespace YouTubeDownloader.Services;
+
+public class VideoDownloadService
 {
-    public class VideoDownloadService
+    public async Task DownloadVideo(string videoUrl, string path, int size)
     {
-        private readonly HttpClient _httpClient;
+        var youtube = new YoutubeClient();
+        var video = await youtube.Videos.GetAsync(videoUrl);
 
-        public VideoDownloadService()
+        var title = video.Title;
+        var duration = video.Duration;
+
+        var filename = MakeValidFileName($"{title}");
+
+        var af = $"{path}\\{filename}.audio.mp4";
+        var vf = $"{path}\\{filename}.video.mp4";
+        var target = $"{path}\\{filename}.mp4";
+
+        if (File.Exists(target))
         {
-            _httpClient = new HttpClient();
+            Console.WriteLine($"File already exists: {af}");
+            return;
         }
 
-        public async Task DownloadVideo(string videoUrl, string path, int size)
+        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
+
+        var a = streamManifest.GetAudioOnlyStreams().Where(s => s.Container == Container.Mp4).GetWithHighestBitrate();
+        var v = streamManifest.GetVideoOnlyStreams().Where(s => s.Container == Container.Mp4).Less(size);
+
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Console.Write($"{filename}.audio:        ");
+        await youtube.Videos.Streams.DownloadAsync(a, af, new Percent());
+        Console.Write("\b\b\b   ");
+        Console.WriteLine();
+        Console.Write($"{filename}.video:        ");
+        await youtube.Videos.Streams.DownloadAsync(v, vf, new Percent());
+        Console.Write("\b\b\b   ");
+        Console.WriteLine();
+        Console.WriteLine("Combining video and audio...");
+        Console.WriteLine();
+
+        var ffmpegPath = @"c:\Program Files\ffmpeg\ffmpeg.exe";
+        await Process.Start(new ProcessStartInfo
         {
-            var youtube = new YoutubeClient();
-            var video = await youtube.Videos.GetAsync(videoUrl);
+            FileName = Path.GetFullPath(ffmpegPath),
+            Arguments = $"-i \"{af}\" -i \"{vf}\" -loglevel error -c:v copy -c:a copy \"{target}\""
+        }).WaitForExitAsync();
 
-            var title = video.Title;
-            var duration = video.Duration;
+        File.Delete(af);
+        File.Delete(vf);
+        Console.WriteLine($"Video downloaded ({duration}) and saved as {target}");
+    }
 
-            var filename = MakeValidFileName($"{title}");
+    public static string MakeValidFileName(string input)
+    {
+        string sanitized = Regex.Replace(input.RemoveDiacritics(), "[^0-9a-zA-Z,\\- '’‘]", " ");
+        sanitized = Regex.Replace(sanitized, @"\s+", " "); // Replace multiple spaces with a single space
 
-            var af = $"{path}\\{filename}.audio.mp4";
-            var vf = $"{path}\\{filename}.video.mp4";
-            var target = $"{path}\\{filename}.mp4";
+        return sanitized.Length > 255 ? sanitized.Substring(0, 255) : sanitized;
+    }
+}
 
-            if (File.Exists(target))
+public class Percent : IProgress<double>
+{
+    public void Report(double value)
+    {
+        var spin = "◟◜◝◞";
+        Console.Write("\b\b\b\b\b\b\b");
+
+        var t = (int)Math.Floor((double)DateTime.Now.Millisecond / 1000 * 4);
+
+        Console.Write(((int)(value * 100)).ToString("D3") + "% " + $"{spin[t]} ");
+    }
+}
+
+public static class Ext
+{
+    public static string RemoveDiacritics(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+
+        var normalizedString = input.Normalize(NormalizationForm.FormD);
+        var stringBuilder = new StringBuilder();
+
+        foreach (var c in normalizedString)
+        {
+            var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+            if (unicodeCategory != UnicodeCategory.NonSpacingMark)
             {
-                Console.WriteLine($"File already exists: {af}");
-                return;
+                stringBuilder.Append(c);
             }
-
-            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
-
-            var a = streamManifest.GetAudioOnlyStreams().Where(s => s.Container == Container.Mp4).GetWithHighestBitrate();
-            var v = streamManifest.GetVideoOnlyStreams().Where(s => s.Container == Container.Mp4).Less(size);
-
-            Console.OutputEncoding = System.Text.Encoding.Unicode;  
-            
-            Console.Write($"{filename}.audio:        ");
-            await youtube.Videos.Streams.DownloadAsync(a, af, new Percent());
-            Console.Write("\b\b\b   ");
-            Console.WriteLine();
-            Console.Write($"{filename}.video:        ");
-            await youtube.Videos.Streams.DownloadAsync(v, vf, new Percent());
-            Console.Write("\b\b\b   ");
-            Console.WriteLine();
-            Console.WriteLine("Combining video and audio...");
-            Console.WriteLine();
-
-            var ffmpegPath = @"c:\Program Files\ffmpeg\ffmpeg.exe";
-            await Process.Start(new ProcessStartInfo
-            {
-                FileName = Path.GetFullPath(ffmpegPath),
-                Arguments = $"-i \"{af}\" -i \"{vf}\" -loglevel error -c:v copy -c:a copy \"{target}\""
-            }).WaitForExitAsync();
-
-            File.Delete(af);
-            File.Delete(vf);
-            Console.WriteLine($"Video downloaded ({duration}) and saved as {target}");
         }
 
-        public static string MakeValidFileName(string input)
-        {
-            string sanitized = Regex.Replace(input.RemoveDiacritics(), "[^0-9a-zA-Z,\\- '’‘]", " ");
-            sanitized = Regex.Replace(sanitized, @"\s+", " "); // Replace multiple spaces with a single space
-
-            return sanitized.Length > 255 ? sanitized.Substring(0, 255) : sanitized;
-        }
+        return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
     }
 
-    public class Percent : IProgress<double>
-    {
-        public void Report(double value)
-        {
-            var spin = "◟◜◝◞";
-            Console.Write("\b\b\b\b\b\b\b");
-
-            var t = (int)Math.Floor((double)DateTime.Now.Millisecond / 1000 * 4);
-
-            Console.Write(((int)(value * 100)).ToString("D3") + "% "+ $"{spin[t]} ");
-        }
-    }
-
-    public static class Ext
-    {
-        public static string RemoveDiacritics(this string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-                return input;
-
-            var normalizedString = input.Normalize(NormalizationForm.FormD);
-            var stringBuilder = new StringBuilder();
-
-            foreach (var c in normalizedString)
-            {
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-                {
-                    stringBuilder.Append(c);
-                }
-            }
-
-            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
-        }
-
-        public static IVideoStreamInfo? Less(this IEnumerable<IVideoStreamInfo> streamInfos, int size) =>
-            streamInfos.Where(s => s.VideoQuality.MaxHeight <= size).OrderByDescending(s => s.VideoQuality).FirstOrDefault();
-    }
+    public static IVideoStreamInfo Less(this IEnumerable<IVideoStreamInfo> streamInfos, int size) =>
+        streamInfos.Where(s => s.VideoQuality.MaxHeight <= size).OrderByDescending(s => s.VideoQuality).FirstOrDefault();
 }
